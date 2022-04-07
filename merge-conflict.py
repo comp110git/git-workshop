f99a23cb694dc62db6affcc0e2e0166ffb6dedd4
Jesse Wei <jessew13@email.unc.edu>
2022-04-07 18:39:01 -0400

    Merge conflict example

---
+++ merge-conflict.py
@@ -0,0 +1,12 @@
+"""This is code from the main branch."""
+
+__author__ = "main branch developer"
+
+
+def main() -> None:
+    print("main branch developer says hi")
+    print("main branch devs are superior btw")
+
+
+if __name__ == "__main__":
+    main()
